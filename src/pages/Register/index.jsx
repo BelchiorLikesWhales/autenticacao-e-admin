0d0bd9537fec42c0bc40a2618377b85428b07b8f
BelchiorLikesWhales <106777905+BelchiorLikesWhales@users.noreply.github.com>
2022-11-07 01:39:58 -0400

    Update index.jsx

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,10 +1,24 @@
+import {useState} from 'react';
+import './register.css'
 
 function Register() {
-    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
-      <div >
-
-      </div>
+        <div className='home-container'>
+            <h1>Lista de Tarefas</h1>
+            <span>Gerencie sua agenda fácil.</span>
+            <form className='form'>
+            <input type="email“ placeholder='seuemail@server.com'
+            value={email} onChange={(e)=>setEmail(e.target.value)}/> <br/>
+            <input type="password“ placeholder='******'
+            value={password}onChange={(e)=>setPassword(e.target.value)}/><br/>
+            <button type="submit">Acessar</button>
+            </form>
+        <Link className="button-Link" to={'/register'}>
+            Não possui uma conta? Cadastre-se
+        </Link>
+        </div>
     )
   }
   
