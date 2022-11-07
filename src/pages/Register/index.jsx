bba7e6a784034b882ab60f744892b83e29bb3d83
BelchiorLikesWhales <106777905+BelchiorLikesWhales@users.noreply.github.com>
2022-11-07 01:44:54 -0400

    Update index.jsx

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,22 +1,31 @@
 import {useState} from 'react';
 import './register.css'
 
+function handleLogin(e){
+    e.preventDefault();
+    if(email !== '' && password !==''){
+        alert("TESTE")
+    }else{
+        alert("Preencha todos os campos!")
+    }
+}
+
 function Register() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     return (
         <div className='home-container'>
-            <h1>Lista de Tarefas</h1>
-            <span>Gerencie sua agenda fácil.</span>
-            <form className='form'>
+            <h1>Cadastre-se</h1>
+            <span>Vamos criar sua conta.</span>
+            <form className='form' onSubmit={handleLogin}>
             <input type="email“ placeholder='seuemail@server.com'
             value={email} onChange={(e)=>setEmail(e.target.value)}/> <br/>
             <input type="password“ placeholder='******'
             value={password}onChange={(e)=>setPassword(e.target.value)}/><br/>
             <button type="submit">Acessar</button>
             </form>
-        <Link className="button-Link" to={'/register'}>
-            Não possui uma conta? Cadastre-se
+        <Link className="button-Link" to={'/'}>
+            Já possui uma conta? Faça login
         </Link>
         </div>
     )
